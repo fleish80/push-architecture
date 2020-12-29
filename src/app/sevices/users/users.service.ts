dd3999d6704eee55ca116c99c494a521f42f2e14
DXF0530 <dmitry.fleish@sbdinc.com>
2020-12-29 10:36:51 +0200

    commit-291220201036

--- src/app/sevices/users/users.service.ts
+++ src/app/sevices/users/users.service.ts
@@ -3,7 +3,7 @@ import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {User} from '../../models/user.model';
 import {Observable} from 'rxjs';
 import {tapResponse} from '@ngrx/component-store';
-import {tap} from 'rxjs/operators';
+import {switchMap, tap} from 'rxjs/operators';
 import {AbstractStore} from '../../store/abstract-store';
 
 export const jsonPlaceHolderUrl = 'https://jsonplaceholder.typicode.com';
@@ -17,16 +17,21 @@ export class UsersService extends AbstractStore<User[]> {
         this.load();
     }
 
-    readonly load = this.effect(() => {
-        return this.fetch()
-            .pipe(
-                tap(() => this.updateLoading(true)),
-                tapResponse(
-                    (users: User[]) => this.updateData(users),
-                    (errorResponse: HttpErrorResponse) => this.updateError(errorResponse),
-                ),
-                tap(() => this.updateLoading(false)),
-            );
+    readonly load = this.effect((trigger$: Observable<void>) => {
+        return trigger$.pipe(
+            tap(() => this.updateLoading(true)),
+            switchMap(() => {
+                return this.fetch()
+                    .pipe(
+                        tapResponse(
+                            (users: User[]) => this.updateData(users),
+                            (errorResponse: HttpErrorResponse) => this.updateError(errorResponse),
+                        ),
+                    );
+            }),
+            tap(() => this.updateLoading(false))
+        );
+
     });
 
     protected fetch(): Observable<User[]> {
